87e05ef8726884927ee306ea3e25405a068c8f20
Chris Price <price.c@gmail.com>
2012-09-11 09:26:47 +0100

    support for recursively loading using commonjs pattern

--- iPhonePropertySearch/Scripts/_references.js
+++ iPhonePropertySearch/Scripts/_references.js
@@ -1,72 +1,133 @@
-﻿intellisense.logMessage("Re-read _references.js ");
+﻿(function () {
+  var DEBUG = 3, WARN = 2, ERROR = 1, NONE = 0;
 
-(function () {
+  intellisense.requirejsLogLevel = WARN;
+
+  if (intellisense.requirejsLogLevel >= DEBUG) {
+    intellisense.logMessage("Re-read _references.js ");
+  }
+
   
-  intellisense.logMessage(print(intellisense, 2, 0));
+  function hashCode() {
+    var hash = 0;
+    if (this.length == 0) return hash;
+    for (i = 0; i < this.length; i++) {
+      char = this.charCodeAt(i);
+      hash = ((hash << 5) - hash) + char;
+      hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+  }
+
   // the list of module functions in the order visual studio has called them
   // this should match the order we require them but if the code has been 
   // modified then it can be wrong
   var modules = [];
 
-  // a lookup of module reference to the index in the modules array. However,
-  // this is derrived from the order the require's indicate they
-  // should have been included in which isn't necessarily accurate
-  var references = {};
-  var referenceCount = 0;
+  var references = [];
+  var referencesByLevel = [];
 
-  var root = true;
-
-  function loadScript(url, onload) {
-    var tag = document.createElement("script");
-    tag.src = url;
-    document.getElementsByTagName("head")[0].appendChild(tag);
-  }
+  var commentRegExp = /(\/\*([\s\S]*?)\*\/|([^:]|^)\/\/(.*)$)/mg,
+    cjsRequireRegExp = /[^.]\s*require\s*\(\s*["']([^'"\s]+)["']\s*\)/g;
 
   function noop() {
   }
 
-  function defineNode() {
-    root = false;
-    define.apply(null, arguments);
-    root = true;
+  function loadScript(url) {
+    var tag = document.createElement("script");
+    tag.src = url + ".js";
+    if (intellisense.requirejsLogLevel >= DEBUG) {
+      intellisense.logMessage("loadScript:" + tag.src);
+    }
+    document.getElementsByTagName("head")[0].appendChild(tag);
   }
 
-  function define(arg) {
-    if (typeof arg === 'function') {
-      intellisense.logMessage("define:function:" + arg);
 
-      // async function definition, save for later ...
-      modules.push(arg);
-      // ... and evaluate it to discover requires
-      arg(define);
-
-      return null;
-    }
-    if (typeof arg === 'string') {
-      intellisense.logMessage("define:string:" + arg);
-
-      var idx = references[arg];
-      if (idx === undefined) {
-        // haven't yet encountered this reference
-        idx = references[arg] = referenceCount++;
-        intellisense.logMessage("define:string:unknown:" + arg);
-      }
-
-      // always load the reference, we have no way of knowing if this is 
-      // the first past i.e. no deps loaded or not. which obv. screws the 
-      // loading order
-      intellisense.logMessage("define:string:loadScript:[" + idx + "]" + arg);
-      loadScript(arg + ".js");
-
-      if (modules.length > 1 && modules[idx]) {
-        // if we've loaded more than just the root module AND
-        // the module is loaded, return it
-        intellisense.logMessage("define:string:module:[" + idx + "]" + arg);
-        return modules[idx](noop);
-      }
-    }
+  function processModule(levelReferences, callback) {
+    callback.toString()
+            .replace(commentRegExp, '')
+            .replace(cjsRequireRegExp, function (match, dep) {
+              if (references.indexOf(dep) === -1) {
+                references.push(dep);
+                levelReferences.push(dep);
+              }
+            });
   }
 
-  window.define = define;
+  window.define = function (module) {
+    if (intellisense.requirejsLogLevel >= DEBUG) {
+      intellisense.logMessage("define:" + modules.length + ":" + hashCode(module.toString()));
+    }
+
+    // a decent test for the end of the scripts seems to be a duplicate script
+    // sadly equality doesn't cut it, so string compare
+    if (modules[modules.length - 1] + "" != module + "") {
+      modules.push(module);
+      return;
+    }
+
+    // remove the root, it should not be considered a module (we still have a reference to it)
+    modules.pop();
+
+    if (intellisense.requirejsLogLevel >= DEBUG) {
+      intellisense.logMessage("define:process:" + modules.length);
+    }
+    var level = 0;
+    var levelReferences = referencesByLevel[level] = [];
+    processModule(levelReferences, module);
+    var index = 0;
+    while (levelReferences.length) {
+      levelReferences = referencesByLevel[++level] = [];
+      if (intellisense.requirejsLogLevel >= DEBUG) {
+        intellisense.logMessage("define:process:level:" + level);
+      }
+      if (index >= modules.length) {
+        if (intellisense.requirejsLogLevel >= WARN) {
+          intellisense.logMessage("define:process:break:" + level + ":" + index + ":" + modules.length);
+        }
+        break;
+      }
+      for (var i = 0; i < referencesByLevel[level - 1].length; i++) {
+        if (intellisense.requirejsLogLevel >= DEBUG) {
+          intellisense.logMessage("define:process:module:" + index);
+        }
+        if (index >= modules.length) {
+          // this should never happen when the correct modules are loaded, it should be caught by the break guard above
+          if (intellisense.requirejsLogLevel >= ERROR) {
+            intellisense.logMessage("define:process:mismatch:" + level + ":" + index + ":" + modules.length);
+          }
+          return;
+        }
+        processModule(levelReferences, modules[index++]);
+      }
+    }
+
+    if (references.length !== modules.length) {
+      if (intellisense.requirejsLogLevel >= WARN) {
+        intellisense.logMessage("define:mismatch:" + references.length + ":" + modules.length);
+      }
+      references.forEach(loadScript);
+      // there's no point continuing, the modules will very likely be mismatched
+      //return;
+    } else {
+      if (intellisense.requirejsLogLevel >= DEBUG) {
+        references.forEach(function (ref, i) {
+          intellisense.logMessage(i + ":" + ref + ":" + modules[i]);
+        });
+      }
+    }
+
+    if (intellisense.requirejsLogLevel >= DEBUG) {
+      intellisense.logMessage("define:evaluate");
+    }
+    module(function require(ref) {
+      var idx = references.indexOf(ref);
+      if (intellisense.requirejsLogLevel >= DEBUG) {
+        intellisense.logMessage("define:evaluate:" + ref + ":" + idx);
+      }
+      var module = modules[idx];
+      return module ? modules[idx](require) : undefinedWithCompletionsOf(null);
+    });
+  };
 
 }());
--- iPhonePropertySearch/Scripts/b.js
+++ iPhonePropertySearch/Scripts/b.js
@@ -1,6 +1,4 @@
 ﻿define(function(require) {
   var a = require("aa");
-  var aa = require("a");
-  var a2 = require("aa");
-  aa.
+  
 });
