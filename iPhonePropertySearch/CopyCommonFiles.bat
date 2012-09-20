b19e0f358de27a9dbe8fd4f874390d9fc2d96f0d
Chris Price <price.c@gmail.com>
2012-09-20 09:20:09 +0100

    fixed paths

--- iPhonePropertySearch/CopyCommonFiles.bat
+++ iPhonePropertySearch/CopyCommonFiles.bat
@@ -1,3 +1,3 @@
-copy ..\HTML5PropertySearch\www\viewModel\*.js viewModel /Y
-copy ..\HTML5PropertySearch\www\model\*.js model /Y
-copy ..\HTML5PropertySearch\www\lib\*.js lib /Y
+copy ..\HTML5PropertySearch\www\viewModel\*.js Scripts/viewModel /Y
+copy ..\HTML5PropertySearch\www\model\*.js Scripts/model /Y
+copy ..\HTML5PropertySearch\www\lib\*.js Scripts/lib /Y
